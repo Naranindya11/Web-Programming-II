b1456feedaf57ab02aa48ab1ace7790bfbf6e508
Naranindya11 <naraanindya2001@gmail.com>
2020-10-19 17:23:59 +0700

    Parktik

---
+++ application/views/v_about.php
@@ -0,0 +1,20 @@
+<section> 
+<h1><?php echo $judul ?></h1> 
+<h4>Nama</h4> 
+<ul type="disc"> 
+    <li>Nama Depan : Nara</li> 
+    <li>Nama Belakang : Anindya Guna</li> 
+</ul> 
+<h4>Alamat</h4> 
+<ul type="disc"> 
+    <li> Jalan Raya Kendayakan Warureja Tegal</li> </ul> 
+    <h4>Tempat Lahir</h4> 
+<ul type="disc"> 
+    <li>Tegal</li> 
+</ul> 
+<h4>Olah Raga Favorit</h4> 
+<ul type="square"> 
+    <li>Bulutangkis</li> 
+    <li>Catur</li> 
+</ul> 
+</section> 
